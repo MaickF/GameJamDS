884f76d7879ca77bb86eaeab6de810867a882a45
Enzo27J <68626004+Enzo27J@users.noreply.github.com>
2023-06-12 21:16:37 -0600

    Módulo del ranking
    Ranking sin el cálculo

--- Frontend/src/app/auth/auth-routing.module.ts
+++ Frontend/src/app/auth/auth-routing.module.ts
@@ -9,6 +9,7 @@ import { Paso2Component } from './paso2/paso2.component';
 import { Paso3Component } from './paso3/paso3.component';
 import { ReporteJuegoComponent } from './reporte-juego/reporte-juego.component';
 import { JuegoComponent } from './juego/juego.component';
+import { RankingComponent } from './ranking/ranking.component';
 
 const routes: Routes = [
   {path: 'login', component: LoginComponent},
@@ -20,6 +21,7 @@ const routes: Routes = [
   {path: 'registro/paso3', component: Paso3Component },
   {path: 'reporteJuego', component: ReporteJuegoComponent },
   {path: 'juego', component: JuegoComponent },
+  {path: 'ranking', component: RankingComponent },
   {path: 'register', redirectTo: 'registro/paso1', pathMatch: 'full' },
 ];
 
